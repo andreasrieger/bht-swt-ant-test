b431289fc90d4e50e8129beb510c83f83d739982
andreasrieger <29732816+andreasrieger@users.noreply.github.com>
2024-06-14 18:00:04 +0200

    added 2 more methods

--- src/calc/Calculator.java
+++ src/calc/Calculator.java
@@ -20,10 +20,9 @@ public class Calculator {
 
 	}
 	
-	public static int add(int a, int b) {
-		return a+b;
-	}
-	
+	public int add(int a, int b) {return a+b;}
+	public int min(int a, int b) { return a-b; }
+	public int mul(int a, int b) { return a*b; }
 	public int div(int a, int b) {
 		return a/b;
 	}
