9b1e7f9d145cdcd4b2e6ea18609ce3304aa83b18
andreasrieger <29732816+andreasrieger@users.noreply.github.com>
2024-06-14 18:29:47 +0200

    merging test branch to main

--- doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"calc","c":"Calculator","l":"add(int, int)","url":"add(int,int)"},{"p":"calc","c":"Calculator","l":"Calculator()","url":"%3Cinit%3E()"},{"p":"calc","c":"Calculator","l":"div(int, int)","url":"div(int,int)"},{"p":"calc","c":"Calculator","l":"main(String[])","url":"main(java.lang.String[])"}]
